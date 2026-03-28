c362289fd22a472e743e0a513195c0aa8a13bb20
ciprianwang-dev <ciprianwang@gmail.com>
2026-03-28 23:22:55 +0200

    merge conflicts?

--- test/test.py
+++ test/test.py
@@ -35,7 +35,6 @@ try:
 except ImportError:
     PYGAME_AVAILABLE = False
 
-<<<<<<< HEAD
 def _ensure_mido_installed():
     """Fast one-time check/install for mido on startup."""
     if importlib.util.find_spec("mido") is not None:
@@ -68,13 +67,11 @@ def _ensure_mido_installed():
     return False
 
 MIDO_AVAILABLE = _ensure_mido_installed()
-=======
 try:
     import winsound
     WINSOUND_AVAILABLE = True
 except ImportError:
     WINSOUND_AVAILABLE = False
->>>>>>> ciprian
 
 # --- Sample-based Piano (uses real piano samples) ---
 SEMITONE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
